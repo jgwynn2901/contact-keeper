1b5c9a24a48ef13cf26c468b130a84abdfa65604
jgwynn2901 <jgwynn2901@yahoo.com>
2020-04-03 00:16:23 -0400

    Added token auth

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
-
 const PORT = process.env.PORT || 5000;
 
-//app.get("/", (req, res) => res.json({ msg: "Welcome to the Contact Keeper API..."}));
+// connect database
+connectDB();
+
+// init middleware
+app.use(express.json({extended: false}));
+
+app.get("/", (req, res) => res.json({ msg: "Welcome to the Contact Keeper API..."}));
 
 // define routes
 app.use('/api/users',require('./routes/users'));
