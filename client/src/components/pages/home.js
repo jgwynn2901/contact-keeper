a8a0fbd8f79aa98dc13f0ff6b7082dc8c610106d
John Gwynn <jgwynn2901@yahoo.com>
2020-11-23 09:35:30 -0500

    work so far...

---
+++ client/src/components/pages/home.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Contacts from '../contacts/Contacts';
+
+const Home = () => {
+  return (
+    <div classNAme="grid-2">
+      <div>
+        {/* ContactForm*/}
+      </div>
+      <div>
+        <Contacts />
+      </div>
+      <h1>Home</h1>
+    </div>
+  )
+}
+
+export default Home;
